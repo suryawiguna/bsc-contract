a5a015998d61712f8bd6c001972921087e89e454
suryawiguna <suryawigunaa@gmail.com>
2021-10-18 15:42:31 +0800

    fix error getLatestPrice (wrong address)

--- contracts/bsc.sol
+++ contracts/bsc.sol
@@ -7,35 +7,38 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract BSC {
     AggregatorV3Interface internal priceFeed;
     using SafeMath for uint256;
-    
+
     /**
      * Network: Binance Smart Chain
      * Aggregator: BNB/USD
-     * Address: 0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE
+     * Address: 0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526
      */
-    constructor() payable {
-        priceFeed = AggregatorV3Interface(0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE);
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526
+        );
     }
-    
-    /**
-     * Returns the latest price
-     */
-    function getLatestPrice() public view returns (int) {
-        (,int price,,,) = priceFeed.latestRoundData();
-        return price;
-    }
-    
+
     uint256 public totalCoinSupply;
     uint256 taxToSeller;
     uint256 taxToDev;
     uint256 taxToBurn;
-    uint256 _decimal = 6;
+    uint256 _decimal = 8;
 
     struct People {
         string name;
         uint256 coinBalance;
+        uint256 usdBalance;
     }
     mapping(string => People) public people;
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int256) {
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return price;
+    }
 
     function setTotalSupply(uint256 _amount) public {
         totalCoinSupply = _amount.mul(10**_decimal);
@@ -48,26 +51,36 @@ contract BSC {
 
     function calcTaxToSeller(uint256 _coinAmount) private {
         // 5% cut from seller
-        people["seller"].coinBalance = people["seller"].coinBalance.sub(_coinAmount);
+        people["seller"].coinBalance = people["seller"].coinBalance.sub(
+            _coinAmount
+        );
     }
 
     function calcTaxToDev(uint256 _coinAmount) private {
         // 3% go to dev & marketing
-        taxToDev = _coinAmount.mul(10**_decimal).mul(3).div(100, "ERROR: _coinAmount is too small").div(10**_decimal);
+        taxToDev = _coinAmount
+            .mul(10**_decimal)
+            .mul(3)
+            .div(100, "ERROR: _coinAmount is too small")
+            .div(10**_decimal);
 
         people["dev"].coinBalance = people["dev"].coinBalance.add(taxToDev);
     }
 
     function calcTaxBurn(uint256 _coinAmount) private {
         // 2% will burn (copped)
-        taxToBurn = _coinAmount.mul(10**_decimal).mul(2).div(100, "ERROR: _coinAmount is too small").div(10**_decimal);
+        taxToBurn = _coinAmount
+            .mul(10**_decimal)
+            .mul(2)
+            .div(100, "ERROR: _coinAmount is too small")
+            .div(10**_decimal);
 
         totalCoinSupply = totalCoinSupply.sub(taxToBurn);
     }
 
     function sell(uint256 _coinAmount) public payable {
         _coinAmount = _coinAmount.mul(10**_decimal);
-        
+
         require(
             _coinAmount <= people["seller"].coinBalance,
             "Not enough coin."
