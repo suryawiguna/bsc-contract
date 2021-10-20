9062da6696c471ba561e1ae06732b7e3bd21984b
suryawiguna <suryawigunaa@gmail.com>
2021-10-20 17:34:56 +0800

    change file name

--- contracts/SellContract.sol
+++ contracts/SellContract.sol
@@ -12,7 +12,7 @@ pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract EXTSellContract {
+contract SellContract {
     AggregatorV3Interface internal priceFeed;
 
     address public owner;
